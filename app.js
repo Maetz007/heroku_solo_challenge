6f061784d5c3c22f204dedee827818a734337cc8
Maetz007 <maetz007@yahoo.com>
2016-06-08 10:24:13 -0500

    first push

---
+++ app.js
@@ -0,0 +1,13 @@
+var http = require("http");
+
+http.createServer( function( req, res ){
+  console.log("Server is wurking");
+  res.writeHead(200); // tells client all is good! 404 = bad
+  res.write("Server is loaded from herokuInClass" + "\n");
+
+  // res.write( modOne(6,7) );
+
+  res.end(); // must call or client/page wont work
+}).listen(9001); // listen must be attached AFTER createServer is ran
+
+console.log("9001... It's over 9000!!!!!");
